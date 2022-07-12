b71d7df4be6a378cd8c2b6dcb613b1cfac14d668
Brianna Birman <brianna.birman@salesforce.com>
2022-07-11 21:38:49 -0700

    SalesforceCommon tests building

--- libs/SalesforceSDKCommon/Package.swift
+++ libs/SalesforceSDKCommon/Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SalesforceSDKCommon",
+            type: .dynamic,
             targets: ["SalesforceSDKCommon"]),
             
     ],
@@ -17,7 +18,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
-    //path: "SalesforceSDKCommon",
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
@@ -34,5 +34,9 @@ let package = Package(
             name: "SalesforceSDKCommonTests",
             dependencies: ["SalesforceSDKCommon"],
             path: "SalesforceSDKCommonTests"),
+        .testTarget(
+            name: "SalesforceSDKCommonSwiftTests",
+            dependencies: ["SalesforceSDKCommonSwift"],
+            path: "SalesforceSDKCommonSwiftTests"),
     ]
 )
