c3aa6e6a4c2deecd335cb00f9fd3fc2ae8a9ed88
Brianna Birman <brianna.birman@salesforce.com>
2025-10-07 17:25:45 -0700

    Add settings menu to login screen

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/UserAccount/SFUserAccountManager.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/UserAccount/SFUserAccountManager.m
@@ -1044,6 +1044,25 @@ static NSString * const kSFGenericFailureAuthErrorHandler = @"GenericFailureErro
     [[NSNotificationCenter defaultCenter] postNotification:loginHostChangedNotification];
     NSString *sceneId = loginViewController.view.window.windowScene.session.persistentIdentifier;
     self.authSessions[sceneId].oauthRequest.loginHost = newLoginHost.host;
+    [self restartAuthenticationForViewController:loginViewController];
+}
+
+- (void)loginViewControllerDidClearCache:(SFLoginViewController *)loginViewController {
+    [SFSDKWebViewStateManager clearCacheWithCompletionHandler:^{}];
+    [self restartAuthenticationForViewController:loginViewController];
+}
+
+- (void)loginViewControllerDidClearCookies:(SFLoginViewController *)loginViewController {
+    [SFSDKWebViewStateManager resetSessionCookie];
+    [self restartAuthenticationForViewController:loginViewController];
+}
+
+- (void)loginViewControllerDidReload:(SFLoginViewController *)loginViewController {
+    [self restartAuthenticationForViewController:loginViewController];
+}
+
+- (void)restartAuthenticationForViewController:(SFLoginViewController *)loginViewController {
+    NSString *sceneId = loginViewController.view.window.windowScene.session.persistentIdentifier;
     [self restartAuthentication:self.authSessions[sceneId]];
 }
 
